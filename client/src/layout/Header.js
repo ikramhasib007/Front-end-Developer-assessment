cfa7a6f7c45530d233e915ebe8c06ac7b9cb4ac4
ikramhasib007 <ikramhasib007@gmail.com>
2022-04-02 17:45:01 +0600

    Header navigation design done

--- client/src/layout/Header.js
+++ client/src/layout/Header.js
@@ -3,12 +3,6 @@ import { SearchIcon } from '@heroicons/react/solid'
 import { BellIcon, MenuIcon, XIcon, CalendarIcon, UsersIcon } from '@heroicons/react/outline'
 import { classNames } from 'src/utils'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
 const navigation = [
   { name: 'For You', href: '#', current: true },
   { name: 'Homes', href: '#', current: false },
@@ -151,23 +145,7 @@ function Header() {
               ))}
             </div>
             <div className="border-t border-gray-200 pt-4 pb-3">
-              <div className="px-4 flex items-center">
-                <div className="flex-shrink-0">
-                  <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-gray-800">{user.name}</div>
-                  <div className="text-sm font-medium text-gray-500">{user.email}</div>
-                </div>
-                <button
-                  type="button"
-                  className="ml-auto flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-              <div className="mt-3 px-2 space-y-1">
+              <div className="px-2 space-y-1">
                 {userNavigation.map((item) => (
                   <Disclosure.Button
                     key={item.name}
