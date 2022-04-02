8d7cbc3b49e027ed557dc389a3ee82eb90f1630f
ikramhasib007 <ikramhasib007@gmail.com>
2022-04-03 01:03:31 +0600

    review render at landing page

---
+++ client/src/routes/home/index.js
@@ -0,0 +1,11 @@
+import Reviews from 'components/reviews'
+
+function Home() {
+  return (
+    <div className='mt-8 space-y-8'>
+      <Reviews />
+    </div>
+  )
+}
+
+export default Home
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,3 +1,11 @@
+import getConfig from "next/config"
+const { publicRuntimeConfig } = getConfig()
+
 export function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
+}
+
+export function getImageSrc({imageUrl = '', images = []} = {}) {
+  let src = imageUrl ? imageUrl : `${publicRuntimeConfig.STATIC_PATH}/${images[images.length - 1]?.path}`
+  return src
 }
